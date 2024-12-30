ad769ceaa4d5d21a1e516564e39f8ae9eb4e3bae
raddva <111165508+raddva@users.noreply.github.com>
2024-12-31 02:34:42 +0700

    add: main_data.csv & modify

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,19 +1,15 @@
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-# from babel.numbers import format_currency
 
 sns.set_theme(style='dark')
 
-st.title('Analisis Air Quality Dataset')
+st.title('Analisis Bike Sharing Dataset')
 
-x = np.random.normal(15, 5, 250)
- 
-fig, ax = plt.subplots()
-ax.hist(x=x, bins=15)
-st.pyplot(fig)
+main_data = pd.read_csv("dashboard/main_data.csv")
+
+st.sidebar.header("Select Analysis")
 
 st.caption('Nadya, 2024')
